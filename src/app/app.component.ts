7b86dc412abe377bf635cdeb5a5c8e1d4d78dc8c
makemile <mkengya@gmail.com>
2025-01-05 16:16:50 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet],
+  template: '<router-outlet/>',
 })
 export class AppComponent {
   title = 'store';
